4308f2ae5b1c0dcf0eccb93d2180edd07cc6dc12
Jurijs Kovzels <jurijs.kovzels@outlook.com>
2021-10-20 23:05:01 +0200

    started working on transliteration

--- build/main.js
+++ build/main.js
@@ -1,17 +1,87 @@
 import { DomUtils } from './Utils/DomUtils.js';
 class LetterMeta {
     index;
+    code;
     letter;
     element;
     width;
-    constructor(index, letter, element, width) {
+    constructor(index, code, letter, element, width) {
         this.index = index;
+        this.code = code;
         this.letter = letter;
         this.element = element;
         this.width = width;
     }
 }
+class ArabicLetterMapping {
+    letter;
+    translit;
+    desription;
+    constructor(letter, translit, desription) {
+        this.letter = letter;
+        this.translit = translit;
+        this.desription = desription;
+    }
+}
+const ArabicMap = {
+    'ا': new ArabicLetterMapping('ا', 'a'),
+    'ب': new ArabicLetterMapping('ب', 'b'),
+    'ت': new ArabicLetterMapping('ت', 't'),
+    'ث': new ArabicLetterMapping('ث', 'th'),
+    'ج': new ArabicLetterMapping('ج', 'j'),
+    'ح': new ArabicLetterMapping('ح', 'ḥ'),
+    'خ': new ArabicLetterMapping('خ', 'kh'),
+    'د': new ArabicLetterMapping('د', 'd'),
+    'ذ': new ArabicLetterMapping('ذ', 'dh'),
+    'ر': new ArabicLetterMapping('ر', 'r'),
+    'ز': new ArabicLetterMapping('ز', 'z'),
+    'س': new ArabicLetterMapping('س', 's'),
+    'ش': new ArabicLetterMapping('ش', 'sh'),
+    'ص': new ArabicLetterMapping('ص', 'ṣ'),
+    'ض': new ArabicLetterMapping('ض', 'ḍ'),
+    'ط': new ArabicLetterMapping('ط', 'ṭ'),
+    'ظ': new ArabicLetterMapping('ظ', 'ẓ'),
+    'ع': new ArabicLetterMapping('ع', '‘'),
+    'غ': new ArabicLetterMapping('غ', 'gh'),
+    'ف': new ArabicLetterMapping('ف', 'f'),
+    'ق': new ArabicLetterMapping('ق', 'q'),
+    'ك': new ArabicLetterMapping('ك', 'k'),
+    'ل': new ArabicLetterMapping('ل', 'l'),
+    'م': new ArabicLetterMapping('م', 'm'),
+    'ن': new ArabicLetterMapping('ن', 'n'),
+    'ة': new ArabicLetterMapping('ة', 'h'),
+    'و': new ArabicLetterMapping('و', 'w'),
+    'ي': new ArabicLetterMapping('ي', 'y'),
+    'ً': new ArabicLetterMapping('ً', '', 'an-nunation')
+};
 class Main {
+    updateTransliterationInput(metas) {
+        let translit = '';
+        for (let i = 0; i < metas.length; i++) {
+            let meta = metas[i];
+            let transliteration = ArabicMap[meta.letter];
+            if (transliteration) {
+                if (transliteration.translit) {
+                    translit += transliteration.translit;
+                }
+                else if (transliteration.translit = 'an-nunation') {
+                    if (i == metas.push.length - 2) {
+                    }
+                }
+                else {
+                    translit += '?';
+                }
+            }
+            else {
+                translit += meta.letter;
+            }
+        }
+        let input = this.getTranslitInput();
+        input.value = translit;
+    }
+    getTranslitInput() {
+        return document.getElementById("nonArabicInput");
+    }
     constructor() {
         DomUtils.docReady(init.bind(this));
         function init() {
@@ -28,7 +98,7 @@ class Main {
         let metas = new Array();
         for (let i = 0; i < elements.length; i++) {
             const element = elements[i];
-            let meta = new LetterMeta(i, element.innerText, element, element.getBoundingClientRect().width);
+            let meta = new LetterMeta(i, element.innerText.charCodeAt(0), element.innerText, element, element.getBoundingClientRect().width);
             metas.push(meta);
         }
         return metas;
@@ -62,6 +132,7 @@ class Main {
         input.replaceChildren(...result);
         let meta = this.constructMeta(result);
         this.updateIsolatedLetters(meta);
+        this.updateTransliterationInput(meta);
         let last = input.lastChild;
         document.getSelection()?.setBaseAndExtent(last, 1, last, 1);
         // for(let i = 0; i < children.length; i++) {
--- sources/main.ts
+++ sources/main.ts
@@ -2,12 +2,82 @@ import {DomUtils} from './Utils/DomUtils.js'
 
 
 class LetterMeta {
-	constructor(public index: number, public letter: string, public element: HTMLElement, public width: number) {
-
+	constructor(public index: number, public code: number, public letter: string, public element: HTMLElement, public width: number) {
 	}
 }
 
+class ArabicLetterMapping {
+	constructor(public letter: string, public translit: string, public desription?: string){
+
+	}
+}
+type LetterMap = {
+	[key: string]: ArabicLetterMapping }
+const ArabicMap: LetterMap = {
+	'ا': new ArabicLetterMapping('ا', 'a'),
+	'ب': new ArabicLetterMapping('ب', 'b'),
+	'ت': new ArabicLetterMapping('ت', 't'),
+	'ث': new ArabicLetterMapping('ث', 'th'),
+	'ج': new ArabicLetterMapping('ج', 'j'),
+	'ح': new ArabicLetterMapping('ح', 'ḥ'),
+	'خ': new ArabicLetterMapping('خ', 'kh'),
+	'د': new ArabicLetterMapping('د', 'd'),
+	'ذ': new ArabicLetterMapping('ذ', 'dh'),
+	'ر': new ArabicLetterMapping('ر', 'r'),
+	'ز': new ArabicLetterMapping('ز', 'z'),
+	'س': new ArabicLetterMapping('س', 's'),
+	'ش': new ArabicLetterMapping('ش', 'sh'),
+	'ص': new ArabicLetterMapping('ص', 'ṣ'),
+	'ض': new ArabicLetterMapping('ض', 'ḍ'),
+	'ط': new ArabicLetterMapping('ط', 'ṭ'),
+	'ظ': new ArabicLetterMapping('ظ', 'ẓ'),
+	'ع': new ArabicLetterMapping('ع', '‘'),
+	'غ': new ArabicLetterMapping('غ', 'gh'),
+	'ف': new ArabicLetterMapping('ف', 'f'),
+	'ق': new ArabicLetterMapping('ق', 'q'),
+	'ك': new ArabicLetterMapping('ك', 'k'),
+	'ل': new ArabicLetterMapping('ل', 'l'),
+	'م': new ArabicLetterMapping('م', 'm'),
+	'ن': new ArabicLetterMapping('ن', 'n'),
+	'ة': new ArabicLetterMapping('ة', 'h'),
+	'و': new ArabicLetterMapping('و', 'w'),
+	'ي': new ArabicLetterMapping('ي', 'y'),
+	'ً': new ArabicLetterMapping('ً', '', 'an-nunation')
+};
+
 class Main {
+	updateTransliterationInput(metas: LetterMeta[]) {
+		
+		let translit = '';
+
+		for(let i = 0; i < metas.length; i++) {
+			let meta = metas[i];
+			let transliteration = ArabicMap[meta.letter];
+			if(transliteration){
+				if(transliteration.translit) {
+					translit += transliteration.translit;
+				}else if (transliteration.translit = 'an-nunation') {
+					if(i == metas.push.length - 2 ){
+
+					}
+				} else {
+					translit += '?';
+				}
+			} else {
+				translit += meta.letter;
+			}
+			
+		}
+		let input = this.getTranslitInput();
+		input.value = translit;
+	}
+
+	getTranslitInput() {
+		return document.getElementById("nonArabicInput") as HTMLInputElement;
+	}
+
+
+
 	constructor(){
 		DomUtils.docReady(init.bind(this));
 
@@ -27,7 +97,7 @@ class Main {
 
 		for (let i = 0; i < elements.length; i++) {
 			const element = elements[i];
-			let meta = new LetterMeta(i, element.innerText, element, element.getBoundingClientRect().width);
+			let meta = new LetterMeta(i, element.innerText.charCodeAt(0), element.innerText, element, element.getBoundingClientRect().width);
 			metas.push(meta);
 		}
 		return metas;
@@ -68,6 +138,7 @@ class Main {
 		let meta = this.constructMeta(result);
 
 		this.updateIsolatedLetters(meta);
+		this.updateTransliterationInput(meta);
 
 		let last = input.lastChild as Node;
 		document.getSelection()?.setBaseAndExtent(last, 1, last, 1);
